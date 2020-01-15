5e5328535464ec5d70398e9cdb49b7919315a1e5
Emery Tate <tateejt@gmail.com>
2020-01-15 06:21:29 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ActiveRecord::Base
   validate :is_title_case
   before_validation :make_title_case
   belongs_to :author
-
+  
+  def last_updated
+    updated_at.strftime("Last updated %A, %b %e, at %l:%M %p")
+  end
+  
   private
 
   def is_title_case
